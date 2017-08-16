827d92008b1c90cc0a5229638962f9fc9517d4b3
Monty Kumar <aryanmk.mk5@gmail.com>
2017-08-16 15:03:35 +0530

    Update omni_hm_note_1w.mk

--- omni_hm_note_1w.mk
+++ omni_hm_note_1w.mk
@@ -7,7 +7,7 @@ PRODUCT_RELEASE_NAME := hm_note_1w
 $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/Xiaomi/hm_note_1w/device_hm_note_1w.mk)
+$(call inherit-product, device/Xiaomi/hm_note_1w/omni_hm_note_1w.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := hm_note_1w
